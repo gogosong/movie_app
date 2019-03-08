e3cfcfef91a62727789f2a7663febb305484b930
talk1365@naver.com <talk1365@naver.com>
2019-03-08 10:24:47 +0900

    Updates

---
+++ precache-manifest.983280f94a7698f3240efd448bbf541c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.7952d9d4.js"
+  },
+  {
+    "revision": "d8d013b4261fd38577fe",
+    "url": "/movie_app/static/js/main.d8d013b4.chunk.js"
+  },
+  {
+    "revision": "ac164eeb7fe39897100f",
+    "url": "/movie_app/static/js/2.ac164eeb.chunk.js"
+  },
+  {
+    "revision": "d8d013b4261fd38577fe",
+    "url": "/movie_app/static/css/main.de8ee1bf.chunk.css"
+  },
+  {
+    "revision": "8d2b75c92b887629fd0612500155eac6",
+    "url": "/movie_app/index.html"
+  }
+];
